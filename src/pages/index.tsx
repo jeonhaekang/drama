92d4eb2e28f29dd327bcd9f119351b9bba594b7f
jeonhaekang <1003gorkd@naver.com>
2023-09-19 14:00:07 +0900

    -

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
   return {
     redirect: {
-      destination: `https://api.shop-pro.jp/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_COLOR_ME_CLIENT_ID}&redirect_uri=http://localhost:3000/home&response_type=code&scope=read_products%20write_products%20read_sales%20write_sales%20read_shop_coupons`,
+      destination: `https://api.shop-pro.jp/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_COLOR_ME_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT}&response_type=code&scope=read_products%20write_products%20read_sales%20write_sales%20read_shop_coupons`,
       statusCode: 302,
     },
   };
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,7 +5,7 @@ export const getToken = async (code: string) => {
       code,
       client_id: process.env.NEXT_PUBLIC_COLOR_ME_CLIENT_ID as string,
       client_secret: process.env.NEXT_PUBLIC_COLOR_ME_CLIENT_PASSWORD as string,
-      redirect_uri: "http://localhost:3000/home",
+      redirect_uri: process.env.NEXT_PUBLIC_REDIRECT as string,
       grant_type: "authorization_code",
     }),
   }).then((res) => res.json() as Promise<{ access_token: string }>);
