b546c5f0b3c3349d4fc1ca699dff6a37e3acede2
jeonhaekang <1003gorkd@naver.com>
2023-09-19 20:18:02 +0900

    -

--- src/utils/downloadCSV.ts
+++ src/utils/downloadCSV.ts
@@ -1,13 +1,16 @@
 import dayjs from "dayjs";
+import { TextEncoder } from "text-encoding";
 
 export const downloadCSV = (content: string) => {
-  const BOM = "\uFEFF"; // UTF-8의 BOM
-  const blob = new Blob([BOM + content], { type: "text/csv;charset=utf-8" });
+  const encoder = new TextEncoder("windows-1252");
+  const encodedContent = encoder.encode(content);
+
+  const blob = new Blob([encodedContent], { type: "text/csv" });
   const url = window.URL.createObjectURL(blob);
   const a = document.createElement("a");
 
   a.href = url;
-  a.download = dayjs(new Date()).format("YYYY-MM-DD HH:mm");
+  a.download = dayjs(new Date()).format("YYYY-MM-DD HH:mm") + ".csv";
   a.click();
 
   window.URL.revokeObjectURL(url);
