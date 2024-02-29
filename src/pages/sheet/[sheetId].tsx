50dd9b8daa381c616d9a083b7b5c8b97557cd961
jeonhaekang <1003gorkd@naver.com>
2024-02-29 20:16:37 +0900

    -

--- src/pages/sheet/[sheetId].tsx
+++ src/pages/sheet/[sheetId].tsx
@@ -6,6 +6,14 @@ import { DownloadCSV, OrderCard, OrderCardSkeleton } from "~/components/sheet";
 import { selectSheet } from "~/server/order";
 import { requiredSession } from "~/utils";
 
+const map = {
+  A: "자동번역",
+  B: "한국 블루레이",
+  C: "중국",
+  D: "한국 디브이디",
+  E: "기타",
+} as const;
+
 const SheetDetail = () => {
   const router = useRouter();
 
@@ -21,29 +29,21 @@ const SheetDetail = () => {
     sale.details.forEach(({ product_name }) => {
       const _type = ["C", "D", "A", "B"];
 
-      let type = product_name[0] as "C" | "D" | "A" | "B" | "E";
+      let type = product_name[0] as keyof typeof map;
 
       if (!_type.includes(type)) {
         type = "E";
       }
 
-      if (!acc[type]) {
-        acc[type] = {};
+      if (!acc[map[type]]) {
+        acc[map[type]] = {};
       }
 
-      acc[type][product_name.trim()] = (acc[type][product_name.trim()] || 0) + 1;
+      acc[map[type]][product_name.trim()] = (acc[map[type]][product_name.trim()] || 0) + 1;
     });
 
     return acc;
-  }, {} as { C: { [key: string]: number }; D: { [key: string]: number }; A: { [key: string]: number }; B: { [key: string]: number }; E: { [key: string]: number } });
-
-  const map = {
-    A: "자동번역",
-    B: "한국 블루레이",
-    C: "중국",
-    D: "한국 디브이디",
-    E: "기타",
-  } as const;
+  }, {} as { [key: string]: { [key: string]: number } });
 
   return (
     <div>
@@ -57,13 +57,13 @@ const SheetDetail = () => {
             <CardHeader>마토메</CardHeader>
 
             {Object.entries(matome)
-              .sort((a, b) => map[a[0] as keyof typeof map].localeCompare(map[b[0] as keyof typeof map]))
+              .sort((a, b) => a[0].localeCompare(b[0]))
               .map(([type, drama]) => (
                 <div key={type}>
                   <Divider />
 
                   <CardBody>
-                    <p className="text-sm text-center mb-3 text-warning-500">{map[type as keyof typeof map]}</p>
+                    <p className="text-sm text-center mb-3 text-warning-500">{type}</p>
 
                     <div className="flex flex-col gap-3">
                       {Object.entries(drama)
