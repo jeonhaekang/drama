0d8be371b0b99043ead9627e9f5c07f9d49b3326
jeonhaekang <1003gorkd@naver.com>
2024-10-31 14:44:39 +0900

    -

--- src/pages/home/components/edit.tsx
+++ src/pages/home/components/edit.tsx
@@ -3,18 +3,29 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 import { supabase } from "~/server/config";
 
-export const Edit = ({ id, title, description }: { id: number; title: string; description: string }) => {
+export const Edit = ({
+  id,
+  title,
+  description,
+  price,
+}: {
+  id: number;
+  title: string;
+  description: string;
+  price: string;
+}) => {
   const queryClient = useQueryClient();
 
   const form = useForm({
     defaultValues: {
       title,
       description,
+      price,
     },
   });
 
   const { mutate } = useMutation({
-    mutationFn: async (form: { title: string; description: string }) => {
+    mutationFn: async (form: { title: string; description: string; price: string }) => {
       const { error } = await supabase.from("yahooList").update(form).eq("id", id);
       if (error) throw error;
     },
@@ -27,6 +38,7 @@ export const Edit = ({ id, title, description }: { id: number; title: string; de
     <form className="flex flex-col gap-2" onSubmit={form.handleSubmit((form) => mutate(form))}>
       <Input {...form.register("title")} />
       <Textarea {...form.register("description")} />
+      <Input {...form.register("price")} />
       <Button type="submit" color="primary">
         수정
       </Button>
